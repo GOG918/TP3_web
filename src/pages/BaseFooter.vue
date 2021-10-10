e19429ee0c09909f557c52a1356e1b4bb9e7e6a3
GOGBE MOUSSA <moussa.abdoul.razak.kone.gogbe@efrei.net>
2021-10-10 23:57:08 +0200

    ex5 commit

---
+++ src/pages/BaseFooter.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+ {{message}}
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseFooter',
+  data (){
+      return{
+          message: 'Here comes the content of the Homepage'
+        }
+  }
+}
---
+++ src/pages/BaseHeader.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+ <p>{{message}}</p>
+ <p>{{username}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeader',
+  data (){
+      return{
+          message: 'Home',
+          username: 'GOGBE MOUSSA'
+        }
+  }
+}
